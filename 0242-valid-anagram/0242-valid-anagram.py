c9602f6af63c2a1d57669ca289452f7df82146a2
Shigekuni <shigekuni.work@gmail.com>
2025-08-10 14:45:29 +0900

    Time: 11 ms (72.29%), Space: 18 MB (34.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,15 @@
 from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # cal spped O(n)
-        # space O(n)
         if len(s) != len(t):
             return False
-        
-        hash_map = defaultdict(int)
+        freq = defaultdict(int)
         for c in s:
-            hash_map[c] += 1
-        
+            freq[c] += 1
         for c in t:
-            if c not in hash_map or hash_map[c] < 1:
+            freq[c] -= 1
+            if freq[c] < 0:
                 return False
-            hash_map[c] -= 1
-        
         return True
 
