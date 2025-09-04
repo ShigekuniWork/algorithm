aab8e16c02c8b7deb71b1d16b59f8b88089adfa3
Koki Shigekuni <shigekuni.work@gmail.com>
2025-09-05 00:26:18 +0900

    Time: 0 ms (100%), Space: 2.1 MB (93.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.rs
+++ 0070-climbing-stairs/0070-climbing-stairs.rs
@@ -1,17 +1,16 @@
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        if n <= 3 {
+        if n < 3 {
             return n;
         }
 
-        let mut prev = 1;
-        let mut curr = 2;
-
-        for _ in 3..=n {
-            let next = prev + curr;
-            prev = curr;
-            curr = next;
+        let (mut prev2, mut prev1) = (2, 3);
+        for _ in 4..=n {
+            let current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
         }
-        curr
+
+        prev1
     }
 }
