b5ebd893aacf8a268c56afff740162aef3f24aeb
Koki Shigekuni <shigekuni.work@gmail.com>
2025-09-04 21:25:36 +0900

    Time: 0 ms (100%), Space: 2.5 MB (39.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,16 +1,18 @@
 use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut cache = HashMap::new();
+        let mut index_cache = HashMap::new();
 
-        for (index, &num) in nums.iter().enumerate() {
-            let diff = target - num;
-            if let Some(&pair_index) = cache.get(&diff) {
-                return vec![pair_index as i32, index as i32];
+        for (i, v) in nums.iter().enumerate() {
+            let diff = target - v;
+
+            if let Some(&pair_index) = index_cache.get(&diff) {
+                return vec![pair_index, i as i32];
             }
-            cache.insert(num, index);
+            index_cache.insert(v, i as i32);
         }
-
+        // Not found pair of two sum
         vec![]
     }
 }
