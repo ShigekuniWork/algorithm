23a4c1390c8e3b1135cc7a5a6fd558a2f9059bd4
Koki Shigekuni <shigekuni.work@gmail.com>
2025-09-07 21:20:03 +0900

    Time: 1 ms (5.56%), Space: 2.7 MB (26.77%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
@@ -0,0 +1,37 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::cmp;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            None => 0,
+            Some(root) => {
+                let current_node = root.borrow();
+
+                let left_count = Solution::max_depth(current_node.left.clone()) + 1;
+                let right_count = Solution::max_depth(current_node.right.clone()) + 1;
+
+                return cmp::max(left_count, right_count);
+            }
+        }
+    }
+}
