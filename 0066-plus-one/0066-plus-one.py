8ef259fa1f14221704f2bc34005de592e56cc61d
Koki Shigekuni <shigekuni.work@gmail.com>
2025-09-17 23:51:45 +0900

    Time: 0 ms (100%), Space: 17.9 MB (8.58%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,24 +2,17 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # I will simulate addition starting from the last digit.
-        # If the digit is 9, set it to 0 and carry over to the next digit on the left.
-        # If all digits are 9, we create a new leading 1.
-        # This algorithm runs in O(N) time.
-        # For space complexity, it is O(1) in the common case since we modify the input list in place.
-        # But in the worst case, when all digits are 9, we create a new array, so it takes O(N) extra space
+        # I will simulate addition from the last digit to the first.
+        # Iterate backwards: if the digit is less than 9, increment and return.
+        # If the digit is 9, set it to 0 and continue carrying over.
+        # If all digits are 9, prepend a new 1 at the front.
+        # This runs in O(N) time and in the worst case O(N) extra space.
 
-        index = len(digits) - 1
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
 
-        # Propagate carry while digits are 9.
-        while index >= 0 and digits[index] == 9:
-            digits[index] = 0
-            index -= 1
-
-        # If index < 0, it means all digits were 9.
-        if index < 0:
-            return [1] + digits
-
-        # Otherwise, just increment the current digit.
-        digits[index] += 1
-        return digits
+        # If we finish the loop, all digits were 9.
+        return [1] + digits
