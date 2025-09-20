dd8c21fe29002cef7b6f51ffd91c8c3393a64098
Koki Shigekuni <shigekuni.work@gmail.com>
2025-09-20 11:51:28 +0900

    Time: 138 ms (16.01%), Space: 27 MB (8.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        result = 0
-        # 1 <= prices.length <= 105
+        # I iterate through list to efficiently solve this problem more than brute force.
+        # Iterate and check max profit and update grater than profit.
+        
+        # if prices list is empty, profit is 0
+        if prices is None:
+            return 0
+        
+        # default maximum profit is 0
+        max_profit = 0
+        
+        # I keep minimal buy price for calculate profit
         buy_price = prices[0]
-
-        for price in prices[1::]:
-            if price < buy_price:
-                buy_price = price
-            else:
-                result = max(result, price - buy_price)
-
-        return result 
-
+        
+        # Iterate from 1 index
+        for current_price in prices[1:]:
+            # check weather less than maximum buy price
+            buy_price = min(current_price, buy_price)
+            
+            # update maximum profit
+            profit = current_price - buy_price
+            max_profit = max(profit, max_profit)
+        
+        return max_profit
+            
