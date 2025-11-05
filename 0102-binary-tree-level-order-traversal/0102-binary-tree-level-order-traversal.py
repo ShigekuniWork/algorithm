030899a6fa42e9bc6174cff14f00a062da03b8ec
Koki Shigekuni <shigekuni.work@gmail.com>
2025-11-05 09:03:54 +0900

    Time: 0 ms (100%), Space: 18.4 MB (59.5%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -7,21 +7,42 @@
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
+        """
+        I'd appach this problem by using BFS.
+        First, I store root to the deque.
+        Second, Iterate thought deque and sotre the value of the current node to result each level
+        """
+        if root is None:
+            return []
 
-        q = collections.deque()
-        q.append(root)
+        # Set up deque to store iterate each of lovels
+        # initial value is current root
+        queue = deque()
+        queue.append(root)
+        # Assing empty list to 'result'
+        result = []
 
-        while q:
-            qLen = len(q)
-            level = []
-            for i in range(qLen):
-                node = q.popleft()
-                if node:
-                    level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if level:
-                res.append(level)
-
-        return res
+        # Iterate thought while existing queue
+        while queue:
+            # Assing empty list to cur_levels
+            cur_levels = []
+            queue_lenght = len(queue)
+            # Iterate though queue to get value of current levles
+            for _ in range(queue_lenght):
+                # Append value poped from the queue
+                cur = queue.popleft()
+                cur_levels.append(cur.val)
+                # if left node, right node of current node, append to queue
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            # then append the result of current levels to result
+            result.append(cur_levels)
+        
+        return result
+        """
+        The time complexity is O(n) because every node in the tree is visited once.
+        The space complexity is O(n) as well because, in the worst case, 
+        the queue can hold all nodes at the widest level of the tree.
+        """
