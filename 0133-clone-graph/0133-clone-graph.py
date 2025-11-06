bb0733e22da8a4da56bdc28f667e8c1c62fcf685
Koki Shigekuni <shigekuni.work@gmail.com>
2025-11-07 07:40:17 +0900

    Time: 38 ms (85.55%), Space: 18.1 MB (56.95%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,23 +5,35 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
-        
-        old_to_new = {}
-        old_to_new[node] = Node(node.val)
-        dq = deque([node])
+        """
+        I'd approach to solve this problem by using DFS.
+        I'll use a hash map to track visited nodes and prevent duplicated deep copies.
+        For each node, I'll create a copy and recursively clone its neighbors.
+        """
+        # Initialize visited node map
+        visited = {}
 
-        while dq:
-            curr = dq.popleft()
-            for nei in curr.neighbors:
-                if nei not in old_to_new:
-                    old_to_new[nei] = Node(nei.val)
-                    dq.append(nei)
-                old_to_new[curr].neighbors.append(old_to_new[nei])
-        
-        return old_to_new[node]
+        # Declare dfs function
+        def dfs(node):
+            # When current node is in visited, return the visited node
+            if node in visited:
+                return visited[node]
+            
+            # Create deep copy of current node
+            copy = Node(node.val)
+            # Record copied node to visited
+            visited[node] = copy
+            # Iterate through current node's neighbors to copy them as well
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
 
+        return dfs(node) if node else None
 
+        """
+        Time complexity is O(V+E) because we need to visit each vertex and edge.
+        Space complexity is O(V) because I used a hash map to store visited nodes.
+        """
