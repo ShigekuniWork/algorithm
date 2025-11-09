b4bb3deec13314ab2cb5bb41cdd1ab11ec0cc102
Koki Shigekuni <shigekuni.work@gmail.com>
2025-11-10 08:00:27 +0900

    Time: 136 ms (18.52%), Space: 26.9 MB (32.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
-        I would approch this problem by iterating though prices
-        and update minmal buy price and sell day
+        I'll approach this problem by iterating through the prices and updating the max profit each iteration. 
         """
+        # Assign 0 to response
+        response = 0
 
-        # Assign 0 to the valiable 'max_profit'
-        max_profit = 0
-        # Assing minimal buy price as first of prices to min_buy_price
+        # Assign price of first day to min_buy_price
         min_buy_price = prices[0]
 
-        # loop start, price over prices
+        # Loop through each price in prices
         for price in prices:
-            # if price less than min_buy_price, update the price
+            # Calculate and update min_buy_price with the minimum of current min_buy_price and price
             min_buy_price = min(min_buy_price, price)
-            # if we can get profit grater than previous max_profit, update the profit
-            max_profit = max(max_profit, price - min_buy_price)
+            # Compare current profit with previous max profit and update response
+            response = max(response, price - min_buy_price)
         
-        return max_profit
+        return response
+        """
+        Time complexity is O(n) because this approach uses a single loop.
+        Space complexity is O(1)
+        """
