18253fd80bb1ddf89cb745d6a8360bbe4fcbe877
Koki Shigekuni <shigekuni.work@gmail.com>
2025-11-13 07:55:32 +0900

    Time: 27 ms (48.23%), Space: 23.3 MB (80.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,33 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        I'd approach to solve this problem by using prefix and suffix products.
-        First, iterate through nums and multiply the prefix (product of all previous elements) 
-        to each position in the response.
-        Then iterate through the reversed list and multiply the suffix (product of all following elements)
-        to each position in the response.
-        """
-        # Assign one to the list value
+        prefix, suffix = 1, 1
         response = [1] * len(nums)
 
-        # Loop start, i over index of nums
-        prefix = 1
+        # prefix
         for i in range(len(nums)):
-            # Multiply prefix to response
             response[i] *= prefix
-            # Update prefix as the result of multiplying prefix and num
             prefix *= nums[i]
 
-        # Suffix loop start as well from end to first
-        suffix = 1
+        # suffix
         for i in range(len(nums)-1, -1, -1):
             response[i] *= suffix
             suffix *= nums[i]
-        
-        # Return list that contains products of array except self
-        return response
 
-        """
-        Time complexity is O(N)
-        Space complexity is O(1)
-        """
+        return response
