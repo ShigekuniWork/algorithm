8454b3ff02dff7df40cb02f043c697252feee57e
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-06 19:35:49 +0900

    Time: 1681 ms (41.75%), Space: 85.2 MB (60.2%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,25 +1,39 @@
+class DSU:
+    def __init__(self, n):
+        self.Parent = list(range(n + 1))
+        self.Size = [1] * (n + 1)
+
+    def find(self, node):
+        if self.Parent[node] != node:
+            self.Parent[node] = self.find(self.Parent[node])
+        return self.Parent[node]
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+        if self.Size[pu] < self.Size[pv]:
+            pu, pv = pv, pu
+        self.Size[pu] += self.Size[pv]
+        self.Parent[pv] = pu
+        return True
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        n, node = len(points), 0
-        dist = [float("INF")] * n
-        visit = [False] * n
-        edges, res = 0, 0
+        n = len(points)
+        dsu = DSU(n)
+        edges = []
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                edges.append((dist, i, j))
 
-        while edges < n - 1:
-            visit[node] = True
-            nextNode = -1
-            for i in range(n):
-                if visit[i]:
-                    continue
-                curDist = (abs(points[i][0] - points[node][0]) + 
-                abs(points[i][1] - points[node][1]))
-
-                dist[i] = min(dist[i], curDist)
-                if nextNode == -1 or dist[i] < dist[nextNode]:
-                    nextNode = i
-            
-            res += dist[nextNode]
-            node = nextNode
-            edges += 1
-        
+        edges.sort()
+        res = 0
+        for dist, u, v in edges:
+            if dsu.union(u, v):
+                res += dist
         return res
