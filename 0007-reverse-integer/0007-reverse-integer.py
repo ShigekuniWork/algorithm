999a24887ad1e98dcdfda93db8f45ec0cafe217c
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-08 23:04:50 +0900

    Time: 42 ms (36.52%), Space: 17.8 MB (56.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,15 +1,26 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        INT_MIN, INT_MAX = -2**31, 2**31
-        sign = 1 if x >= 0 else -1
-        x = abs(x)
-        result = 0
-        while x:
-            x, digit = divmod(x, 10)
-            if INT_MAX//10 < sign or (result == INT_MAX // 10 and digit > INT_MAX % 10):
-                return 0
-            
-            result = result * 10 + digit
+        INT_MAX = (1 << 31) - 1  # 2147483647
+        INT_MIN = -(1 << 31)     # -2147483648
+
+        reversed_value = 0
         
-        result *= sign
-        return result if INT_MIN <= result <= INT_MAX else 0
+        # 反転処理を行うための、x の絶対値コピー
+        remaining_digits = abs(x) 
+        
+        while remaining_digits != 0:
+            if reversed_value > INT_MAX // 10:
+                return 0
+
+            current_digit = remaining_digits % 10
+            remaining_digits //= 10 
+
+            reversed_value = reversed_value * 10 + current_digit
+            
+        if x < 0:
+            reversed_value *= -1
+
+        if reversed_value < INT_MIN or reversed_value > INT_MAX:
+            return 0
+             
+        return reversed_value
