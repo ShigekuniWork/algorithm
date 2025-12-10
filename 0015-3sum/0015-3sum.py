de1787b482289f666a111f5f08fac4573df6ef41
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-10 22:41:10 +0900

    Time: 414 ms (94.43%), Space: 20.6 MB (70.73%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,63 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        """
-        I would approach this problem by using two-pointer technique.
-        First, I sort the list to make it easy to handle duplicates.
-        Then, for each number, use two pointers to find the other two numbers that
-        sum up to zero
-        """
-        # First, sort the array so that duplicate elements can be skkiped easliy
-        # and two-pointer movement is predictable.
+        res = []
         nums.sort()
 
-        # Initialize response to store valid triplets
-        response = []
-
-        # Iterate through array with both index 'i' and value 'cur'
-        for i, cur in enumerate(nums):
-            # If the currenet value is greter than zero, stop the loop
-            # because all remaining numbers are positive and cannot the sum zero.
-            if cur > 0:
+        for i, num in enumerate(nums):
+            if num > 0:
                 break
             
-            # Skip the number that it is the same as previous one to avoid duplicat results
-            if i > 0 and cur == nums[i-1]:
+            if i > 0 and num == nums[i - 1]:
                 continue
             
-            # set up two-pointer
-            # left stats after 'i'
-            left = i + 1
-            # 'right' stats from the end.
-            right = len(nums) -1
-
-            # Continue while 'left' is less than 'right'
+            left, right = i + 1, len(nums) - 1
             while left < right:
-                # Calucurate total
-                total = cur + nums[left] + nums[right]
-
-                # If the total is less than zero, move 'right' one step left to decrease the sum
+                total = num + nums[left] + nums[right]
                 if total > 0:
                     right -= 1
-                # If the total is greater than zerom 'left' one step right to increase to the sum
                 elif total < 0:
                     left += 1
-                # When the total equals zero, a valid triplet is found
                 else:
-                    response.append([cur, nums[left], nums[right]])
-                    # Move both pointers inward after storing the result.
+                    res.append([num, nums[left], nums[right]])
                     left += 1
                     right -= 1
-
-                    # Skip duplicate elements for the the 'left' pointer
-                    while left < right and nums[left] == nums[left - 1]:
+                    while nums[left] == nums[left - 1] and left < right:
                         left += 1
-                
-            
-        return response
-        """
-        The time complexity is O(n squared) because for each number,
-        a two-pointer scan goes through the rest of the array linearly,
-        and sorting doesn’t dominate that cost.
-        The space complexity is constant because the algorithm only uses
-        a few pointers and modifies the array in place without extra data structures.
-        """
+        
+        return res
