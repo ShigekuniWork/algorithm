8df1be4f9811d1d82e618ffede047e229ac71812
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-12 07:59:57 +0900

    Time: 0 ms (100%), Space: 18.1 MB (66.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,10 +1,11 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        left = 0
+        right = len(nums) - 1
 
         while left <= right:
-            mid = left + (right - left) // 2
-            if target == nums[mid]:
+            mid = (left + right) // 2
+            if nums[mid] == target:
                 return mid
             
             if nums[left] <= nums[mid]:
@@ -17,5 +18,4 @@ class Solution:
                     left = mid + 1
                 else:
                     right = mid - 1
-            
         return -1
