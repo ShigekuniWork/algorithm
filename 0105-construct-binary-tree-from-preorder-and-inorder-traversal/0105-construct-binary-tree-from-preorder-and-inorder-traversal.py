c4dae0ef1c50f81378a3ff7f6cfe77f063884d73
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-14 16:40:29 +0900

    Time: 2 ms (90.06%), Space: 18.5 MB (99.03%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,32 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        indices = {v: i for i, v in enumerate(inorder)}
-        
-        self.pre_idx = 0
-        def dfs(left: int , right: int):
-            if left > right:
-                return
-            
-            root_val = preorder[self.pre_idx]
-            self.pre_idx += 1
-            root = TreeNode(root_val)
-            mid = indices[root_val]
-            
-            root.left = dfs(left, mid - 1)
-            root.right = dfs(mid + 1, right)
+        self.preIndex = 0
+        self.InIndex = 0
 
-            return root
-        
-        return dfs(0, len(preorder) -1)
+        def dfs(limit):
+            if self.preIndex >= len(preorder):
+                return None
+            if inorder[self.InIndex] == limit:
+                self.InIndex += 1
+                return None
+            
+            node = TreeNode(preorder[self.preIndex])
+            self.preIndex += 1
+            node.left = dfs(node.val)
+            node.right = dfs(limit)
 
-        
+            return node
+
+        return dfs(float('INF'))
