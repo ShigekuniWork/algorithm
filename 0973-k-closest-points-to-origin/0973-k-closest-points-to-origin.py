75fa655185847dba3f47ba921c1dbcf53a81ebd5
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-16 07:15:44 +0900

    Time: 83 ms (32.56%), Space: 23.5 MB (10.03%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,24 +1,14 @@
 class Solution:
-    def kClosest(self, points, k):
-        euclidean = lambda x: x[0] ** 2 + x[1] ** 2
-        def partition(l, r):
-            pivotIdx = r
-            pivotDist = euclidean(points[pivotIdx])
-            i = l
-            for j in range(l, r):
-                if euclidean(points[j]) <= pivotDist:
-                    points[i], points[j] = points[j], points[i]
-                    i += 1
-            points[i], points[r] = points[r], points[i]
-            return i
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append([dist, x, y])
 
-        L, R = 0, len(points) - 1
-        pivot = len(points)
+        heapq.heapify(minHeap)
+        res = [[] for _ in range(k)]
+        for i in range(k):
+            _, x, y = heapq.heappop(minHeap)
+            res[i] = [x, y]
 
-        while pivot != k:
-            pivot = partition(L, R)
-            if pivot < k:
-                L = pivot + 1
-            else:
-                R = pivot - 1
-        return points[:k]
+        return res
