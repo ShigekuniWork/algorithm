3276929f895344657a19cbdb939eeb22081b7478
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-19 08:28:39 +0900

    Time: 1379 ms (30.33%), Space: 65.2 MB (77.05%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,36 +1,36 @@
 class TrieNode:
     def __init__(self):
+        self.is_word = False
         self.children = {}
-        self.word = False
-
 
 class WordDictionary:
     def __init__(self):
         self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
-        cur = self.root
+        curr = self.root
         for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
-        cur.word = True
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.is_word = True
 
     def search(self, word: str) -> bool:
-        def dfs(j, root):
-            cur = root
+        def dfs(root, index):
+            curr = root
 
-            for i in range(j, len(word)):
+            for i in range(index, len(word)):
                 c = word[i]
                 if c == ".":
-                    for child in cur.children.values():
-                        if dfs(i + 1, child):
+                    for child in curr.children.values():
+                        if dfs(child, i + 1):
                             return True
                     return False
-                else:
-                    if c not in cur.children:
+                else:   
+                    if c not in curr.children:
                         return False
-                    cur = cur.children[c]
-            return cur.word
+                    curr = curr.children[c]
+            return curr.is_word
 
-        return dfs(0, self.root)
+        return dfs(self.root, 0)
+                
