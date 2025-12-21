e2976c64513e8d9547fb1274edf30c14ffd159f1
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-21 12:48:33 +0900

    Time: 15 ms (89.08%), Space: 17.3 MB (100%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        ROWS, COLS = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
         area = 0
 
         def bfs(r, c):
@@ -12,19 +11,16 @@ class Solution:
 
             while q:
                 row, col = q.popleft()
-                for dr, dc in directions:
-                    nr, nc = dr + row, dc + col
-                    if (nr < 0 or nc < 0 or nr >= ROWS or
-                        nc >= COLS or grid[nr][nc] == 0
-                    ):
-                        continue
-                    q.append((nr, nc))
-                    grid[nr][nc] = 0
-                    res += 1
+                for dr, dc in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        q.append((nr, nc))
+                        grid[nr][nc] = 0
+                        res += 1
             return res
-
-        for r in range(ROWS):
-            for c in range(COLS):
+        
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 1:
                     area = max(area, bfs(r, c))
 
