b38cd684fdaaaa2e542d50fbf6bc025c5b905acc
Koki Shigekuni <shigekuni.work@gmail.com>
2025-12-23 22:21:57 +0900

    Time: 6 ms (50.77%), Space: 18.3 MB (99.59%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -3,22 +3,23 @@ class Solution:
         indegree = [0] * numCourses
         adj = [[] for _ in range(numCourses)]
 
-        for src, dst in prerequisites:
-            indegree[dst] += 1
-            adj[src].append(dst)
+        for start, end in prerequisites:
+            indegree[end] += 1
+            adj[start].append(end)
         
         q = deque()
-        for n in range(numCourses):
-            if indegree[n] == 0:
-                q.append(n)
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
 
         finish = 0
         while q:
-            node = q.popleft()
+            end = q.popleft()
             finish += 1
-            for nei in adj[node]:
+
+            for nei in adj[end]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-
+        
         return finish == numCourses
