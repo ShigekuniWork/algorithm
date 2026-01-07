9a7167f4fff0c1dc04352c1473ced8b84b10decb
Koki Shigekuni <shigekuni.work@gmail.com>
2026-01-08 07:29:41 +0900

    Time: 0 ms (100%), Space: 19.6 MB (5.6%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,18 +1,19 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
-        words = set(wordDict)
-        n = len(s)
-        max_len = max(map(len, words)) if words else 0
-        dp = [False]*(n+1)
-        dp[0] = True
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {len(s): True}
 
-        for i in range(n):
-            if not dp[i]:
-                continue
-            end = min(n, i + max_len)
-            for j in range(i+1, end+1):
-                if s[i:j] in words:
-                    dp[j] = True
-                    if dp[n]:
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+
+            for w in wordDict:
+                end = i + len(w)
+                if end <= len(s) and s[i:end] == w:
+                    if dfs(i + len(w)):
+                        memo[i] = True
                         return True
-        return dp[n]
+
+            memo[i] = False
+            return False
+
+        return dfs(0)
