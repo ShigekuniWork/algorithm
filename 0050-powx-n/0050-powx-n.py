840a0333345956e904d093ade800763ee449b56d
Koki Shigekuni <shigekuni.work@gmail.com>
2026-02-04 08:59:49 +0900

    Time: 0 ms (100%), Space: 19.4 MB (44.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -7,11 +7,10 @@ class Solution:
         
         res = 1
         power = abs(n)
-
         while power:
             if power & 1:
                 res *= x
             x *= x
             power >>= 1
-
+        
         return res if n >= 0 else 1 / res
