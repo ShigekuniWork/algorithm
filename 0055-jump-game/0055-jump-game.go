bd9f30a1b62e086abd20b0f5f965e57a37e33222
Koki Shigekuni <shigekuni.work@gmail.com>
2026-02-17 07:37:26 +0900

    Time: 0 ms (100%), Space: 9.1 MB (55.4%) - LeetHub

--- 0055-jump-game/0055-jump-game.go
+++ 0055-jump-game/0055-jump-game.go
@@ -1,7 +1,7 @@
 func canJump(nums []int) bool {
     goal := len(nums) - 1
 
-    for i := len(nums) -1; i > -1; i-- {
+    for i := len(nums) -1; i >= 0; i-- {
         if nums[i] + i >= goal {
             goal = i
         }
