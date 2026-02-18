cd63da5c336d62f95ab301d089276b70fa2fcdbd
Koki Shigekuni <shigekuni.work@gmail.com>
2026-02-18 21:07:08 +0900

    Time: 1 ms (49.25%), Space: 21.4 MB (62.59%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,17 +3,15 @@ class Solution:
         res = []
 
         for i in range(len(intervals)):
-            start, end = intervals[i]
-            if newInterval[1] < start:
+            if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
-            elif end < newInterval[0]:
-                res.append([start, end])
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
             else:
                 newInterval = [
-                    min(newInterval[0], start),
-                    max(newInterval[1], end),
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
                 ]
-        
         res.append(newInterval)
         return res
